4fc044c5c1927f7ea99972a219982503b6fe8062
cranis <cranis2@naver.com>
2025-06-02 12:42:06 +0900

    wefewfewfwefewfewfwefwefefwefwweefwfewef

--- src/views/dashboard/admin/components/TransactionTable.vue
+++ src/views/dashboard/admin/components/TransactionTable.vue
@@ -77,6 +77,8 @@ export default {
       console.log(recentSuccessedChatHistory.data)
 
 
+      this.$store.commit("aimedison/setRecentScsdChatHistoryOfSlctdUser", recentSuccessedChatHistory.data);
+
 
 
 
