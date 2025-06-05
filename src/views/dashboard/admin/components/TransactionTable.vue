734232ac75fb0c9e3d76f244e7ac36e0d5623549
cranis <cranis2@naver.com>
2025-06-05 11:31:00 +0900

    ewfefwefefwweffweewfefwewf

--- src/views/dashboard/admin/components/TransactionTable.vue
+++ src/views/dashboard/admin/components/TransactionTable.vue
@@ -10,7 +10,7 @@
         ¥{{ scope.row.price | toThousandFilter }}
       </template>
     </el-table-column> -->
-    <el-table-column label="" width="50" align="center">
+    <el-table-column label="" width="100" align="center">
 
 
       <template slot-scope="{row}">
