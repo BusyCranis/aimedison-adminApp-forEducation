fb80a38eb052db215702fc0bbd13ee4330fe8ab8
cranis <cranis2@naver.com>
2025-06-08 11:06:59 +0900

    fewfewfwefewfewefwfewewfefwewf

--- src/views/dashboard/admin/index.vue
+++ src/views/dashboard/admin/index.vue
@@ -931,6 +931,12 @@ export default {
 
     async resetKeywordTrialCount() {
       console.log(this.$store.state.aimedison.currentSelectedUserId)
+
+      const confirmPrompt = "정말로 " + this.$store.state.aimedison.currentSelectedUserId + "님의 대화 내역을 리셋 하시겠습니까?";
+
+
+
+
       const resultAfterReset = await axios.post("/resetaftersuccess", {
         shopId: window.cafe24aimedisonkeywordappshopid,
         userId: this.$store.state.aimedison.currentSelectedUserId
