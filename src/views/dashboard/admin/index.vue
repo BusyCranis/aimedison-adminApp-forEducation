cbc90dc287a8c61c776d131217c7c241d4369134
cranis <cranis2@naver.com>
2025-06-08 11:09:17 +0900

    efwweffeefwefwweffwefewefwefwwefwef

--- src/views/dashboard/admin/index.vue
+++ src/views/dashboard/admin/index.vue
@@ -931,30 +931,22 @@ export default {
 
     async resetKeywordTrialCount() {
       console.log(this.$store.state.aimedison.currentSelectedUserId)
-
       const confirmPrompt = "정말로 " + this.$store.state.aimedison.currentSelectedUserId + "님의 대화 내역을 리셋 하시겠습니까?";
-
-
-
-
-      const resultAfterReset = await axios.post("/resetaftersuccess", {
-        shopId: window.cafe24aimedisonkeywordappshopid,
-        userId: this.$store.state.aimedison.currentSelectedUserId
-      });
-
-
-
-      console.log(resultAfterReset);
-      console.log(resultAfterReset.data);
-
 
       
 
+      if(this.useConfirmHandler(confirmPrompt) === true) {
+        const resultAfterReset = await axios.post("/resetaftersuccess", {
+          shopId: window.cafe24aimedisonkeywordappshopid,
+          userId: this.$store.state.aimedison.currentSelectedUserId
+        });
 
 
 
+        console.log(resultAfterReset);
+        console.log(resultAfterReset.data);
 
-
+      }
 
 
     },
