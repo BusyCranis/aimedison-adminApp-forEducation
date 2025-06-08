7fb2f97c3344e6b6b61d12f074888c72ba118c68
cranis <cranis2@naver.com>
2025-06-08 12:19:11 +0900

    ewfefwewfefwwefefwewfewfwefewfewfefwefwewffewefw

--- src/views/example/components/ArticleDetail.vue
+++ src/views/example/components/ArticleDetail.vue
@@ -317,14 +317,17 @@ export default {
 
 
 
-      if(document.querySelectorAll(".cell")) {
-        if(document.querySelectorAll(".cell").length > 0) {
-          for(let i = 0; document.querySelectorAll(".cell").length; i++) {
-            document.querySelectorAll(".cell")[i].style.paddingLeft = "0px";
-            document.querySelectorAll(".cell")[i].style.paddingRight = "0px";
+      setTimeout(() => {
+        if(document.querySelectorAll(".cell")) {
+          if(document.querySelectorAll(".cell").length > 0) {
+            for(let i = 0; document.querySelectorAll(".cell").length; i++) {
+              document.querySelectorAll(".cell")[i].style.paddingLeft = "0px";
+              document.querySelectorAll(".cell")[i].style.paddingRight = "0px";
+            }
           }
         }
-      }
+      }, 25);
+      
 
     },
 
@@ -446,15 +449,21 @@ export default {
 
 
 
-
-      if(document.querySelectorAll(".cell")) {
-        if(document.querySelectorAll(".cell").length > 0) {
-          for(let i = 0; document.querySelectorAll(".cell").length; i++) {
-            document.querySelectorAll(".cell")[i].style.paddingLeft = "0px";
-            document.querySelectorAll(".cell")[i].style.paddingRight = "0px";
+      setTimeout(() => {
+        if(document.querySelectorAll(".cell")) {
+          if(document.querySelectorAll(".cell").length > 0) {
+            for(let i = 0; document.querySelectorAll(".cell").length; i++) {
+              document.querySelectorAll(".cell")[i].style.paddingLeft = "0px";
+              document.querySelectorAll(".cell")[i].style.paddingRight = "0px";
+            }
           }
         }
-      }
+      }, 25);
+
+    
+
+
+
 
     },
 
